3a67f9b52650a6f42abac9650e82cfcee987e400
Audra Dagenais <audluc44@gmail.com>
2020-09-17 18:34:46 -0700

    fixed errors

--- app.js
+++ app.js
@@ -24,108 +24,128 @@ function chooseRole() {
     .then((answers) => {
       switch (answers.role) {
         case "Manager":
-           addManager()
+          addManager();
           break;
         case "Engineer":
-             addEngineer()
+          addEngineer();
           break;
         case "Intern":
-           addIntern()
+          addIntern();
           break;
         default:
           buildTeam();
       }
     });
 }
-function addManager(){
-    inquirer
+function addManager() {
+  inquirer
     .prompt([
       {
         type: "input",
         name: "eName",
         message: "What is your name?",
-    },
-    {
-        type: "input",
-        name: "eId",
-        message: "What is your ID?",
-    },
-    {
-        type: "input",
-        name: "eEmail",
-        message: "What is your email?",
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is Github name?",
-    },
-]).then((answers) => {
-    let manager = new Manager(answers.eName, answers.eId, answers.eEmail, answers.github)
-    employees.push(manager)
-    chooseRole()
-})
-function addEngineer (){
-    inquirer
-    .prompt([
+      },
       {
         type: "input",
-        name: "eName",
-        message: "What is your name?",
-    },
-    {
-        type: "input",
         name: "eId",
         message: "What is your ID?",
-    },
-    {
+      },
+      {
         type: "input",
         name: "eEmail",
         message: "What is your email?",
-    },
-    {
+      },
+      {
         type: "input",
         name: "github",
         message: "What is Github name?",
-    },
-]).then((answers) => {
-    let engineer = new Engineer(answers.eName, answers.eId, answers.eEmail, answers.github)
-    employees.push(engineer)
-    chooseRole()
-})
-    function addIntern(){
-        inquirer
-        .prompt([
-          {
-            type: "input",
-            name: "eName",
-            message: "What is your name?",
+      },
+    ])
+    .then((answers) => {
+      let manager = new Manager(
+        answers.eName,
+        answers.eId,
+        answers.eEmail,
+        answers.github
+      );
+      employees.push(manager);
+      chooseRole();
+    });
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "eName",
+          message: "What is your name?",
         },
         {
-            type: "input",
-            name: "eLocation",
-            message: "What is your location?",
+          type: "input",
+          name: "eId",
+          message: "What is your ID?",
         },
         {
-            type: "input",
-            name: "eEmail",
-            message: "What email?",
+          type: "input",
+          name: "eEmail",
+          message: "What is your email?",
         },
         {
-            type: "input",
-            name: "eSchool",
-            message: "What school did you attend?",
+          type: "input",
+          name: "github",
+          message: "What is Github name?",
         },
-
-]).then((answers) => {
-    let Intern = new Intern(answers.eName, answers.eLocation, answers.eEmail, answers.eSchool)
-    employees.push(Intern)
-    chooseRole()
-})
-    function buildTeam (){
-    if (!fs.existsSync(OUTPUT_DIR)){
-      fs.mkdirSync(OUTPUT_DIR)
+      ])
+      .then((answers) => {
+        let engineer = new Engineer(
+          answers.eName,
+          answers.eId,
+          answers.eEmail,
+          answers.github
+        );
+        employees.push(engineer);
+        chooseRole();
+      });
+  }
+  function addIntern() {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "eName",
+          message: "What is your name?",
+        },
+        {
+          type: "input",
+          name: "eLocation",
+          message: "What is your location?",
+        },
+        {
+          type: "input",
+          name: "eEmail",
+          message: "What email?",
+        },
+        {
+          type: "input",
+          name: "eSchool",
+          message: "What school did you attend?",
+        },
+      ])
+      .then((answers) => {
+        let Intern = new Intern(
+          answers.eName,
+          answers.eLocation,
+          answers.eEmail,
+          answers.eSchool
+        );
+        employees.push(Intern);
+        chooseRole();
+      });
+  }
+  function buildTeam() {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
     }
-    fs.writeFileSync(outputPath, render(employees), "utf8")
-    }}
-   chooseRole()
+    fs.writeFileSync(outputPath, render(employees), "utf8");
+  }
+}
+chooseRole();
